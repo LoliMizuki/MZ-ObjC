ec9a8171863edb736eaea8e9ad0d1d177103ccb0
Mizuki <lolimizuki@gmail.com>
2016-03-03 10:12:44 +0800

    UIColor isEqualToColor() 改成 category

--- MZUIExtensions.h
+++ MZUIExtensions.h
@@ -1,35 +1,16 @@
 //
 //  MZUIExtensions.h
-//  MZ-ObjC
+//  AsenseTek
 //
-//  Created by Inaba Mizuki on 2016/3/2.
+//  Created by Inaba Mizuki on 2016/3/3.
 //  Copyright © 2016年 Inaba Mizuki. All rights reserved.
 //
 
-#ifndef MZUIExtensions_h
-#define MZUIExtensions_h
-
-#import <OpenGLES/ES2/gl.h>
-#import <CoreGraphics/CoreGraphics.h>
 #import <UIKit/UIKit.h>
 
-NS_INLINE BOOL UIColorIsEqual(UIColor* color1, UIColor* color2, BOOL ignoreAlpha) {
-    CGFloat tolerance = 0.01;
+@interface UIColor (MZUIExtensions)
 
-    CGFloat r1, g1, b1, a1;
-    CGFloat r2, g2, b2, a2;
+- (BOOL)isEqualToColor:(UIColor *)anotherColor ignoreAlpha:(BOOL)ignoreAlpha;
+- (BOOL)isEqualToColor:(UIColor *)anotherColor;
 
-    [color1 getRed:&r1 green:&g1 blue:&b1 alpha:&a1];
-    [color2 getRed:&r2 green:&g2 blue:&b2 alpha:&a2];
-
-    BOOL alphaResult = (ignoreAlpha)? true : (fabs(a1 - a2) < tolerance);
-
-    return  (fabs(r1 - r2) < tolerance) &&
-    (fabs(g1 - g2) < tolerance) &&
-    (fabs(b1 - b2) < tolerance) &&
-    alphaResult;
-}
-
-
-
-#endif /* MZUIExtensions_h */
+@end
---
+++ MZUIExtensions.m
@@ -0,0 +1,34 @@
+//
+//  MZUIExtensions.m
+//  AsenseTek
+//
+//  Created by Inaba Mizuki on 2016/3/3.
+//  Copyright (c) 2013年 Inaba Mizuki. All rights reserved.
+//
+
+#import "MZUIExtensions.h"
+
+@implementation UIColor (MZUIExtensions)
+
+- (BOOL)isEqualToColor:(UIColor *)anotherColor ignoreAlpha:(BOOL)ignoreAlpha {
+    CGFloat tolerance = 0.01;
+
+    CGFloat r1, g1, b1, a1;
+    CGFloat r2, g2, b2, a2;
+
+    [self getRed:&r1 green:&g1 blue:&b1 alpha:&a1];
+    [anotherColor getRed:&r2 green:&g2 blue:&b2 alpha:&a2];
+
+    BOOL alphaResult = (ignoreAlpha)? true : (fabs(a1 - a2) < tolerance);
+
+    return (fabs(r1 - r2) < tolerance) &&
+           (fabs(g1 - g2) < tolerance) &&
+           (fabs(b1 - b2) < tolerance) &&
+           alphaResult;
+}
+
+- (BOOL)isEqualToColor:(UIColor *)anotherColor {
+    return [self isEqualToColor:anotherColor ignoreAlpha:true];
+}
+
+@end
